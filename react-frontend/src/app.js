8db60800ec1836d188ce491983b951631929b625
Jocarfl <jcarreresjr@hotmail.com>
2022-05-19 21:26:19 +0200

    router

--- react-frontend/src/app.js
+++ react-frontend/src/app.js
@@ -1,10 +1,12 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './components/LoginBox'
 
 function App(){
 return(
 <Routes>
 
-   <Route path='/'></Route>
+   <Route path='/' element={<Login/>}></Route>
+   
 
 </Routes>
 
